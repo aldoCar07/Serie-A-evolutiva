cac21caef1e5d534fe504ba6f063a994a6ee5b57
aldoCar07 <aldo.ecj@gmail.com>
2023-10-30 09:23:23 -0600

    Update EjemploChatGPT.py

--- EjemploChatGPT.py
+++ EjemploChatGPT.py
@@ -7,6 +7,7 @@ Title: Serie A calendario
 import pandas as pd
 import random
 import math
+import numpy as np
 
 # Cargar datos de estadios y equipos desde el archivo CSV
 estadios_df = pd.read_csv("estadios.csv")
@@ -51,30 +52,39 @@ def generar_calendario_aleatorio():
     # Implementa la lógica para generar un calendario aleatorio
     pass
 
-# Define una función de aptitud para evaluar los calendarios
+# Define una función de aptitud para evaluar los calendarios, entre menor sea el valor de aptitud, mejor es el calendario
 def evaluar_calendario(calendario):
     # Implementa la función de evaluación para evaluar el calendario de partidos
     # Vamos a evaluar de forma negativa si hay dos partidos en la misma ciudad en la misma jornada
     for jornada in calendario:  #para cada jornada del calendario se hará la evaluación
         ciudades = [] #agregamos las ciudades en las que se juegan los partidos de la jornada (al final debería haber 10 distintos)
         for partido in jornada['partidos']:
-            ciudad_del_estadio = (estadios_df[estadios_df['Stadium'] == partido[2]]).head(1)['City'].values[1] #chorizo para sacar el string de la ciudad en la que se juega un partido de la jornada
-            if(ciudad_del_estadio in ciudades): # si la ciudad ya está agregada, habrá al menos dos partidos en la misma ciudad, así que evaluamos con -1
-                return -1
-            else:
+            ciudad_del_estadio = (estadios_df[estadios_df['Stadium'] == partido[2]]).head(1)['City'].values[0] #chorizo para sacar el string de la ciudad en la que se juega un partido de la jornada
+            if(ciudad_del_estadio in ciudades): # si la ciudad ya está agregada, habrá al menos dos partidos en la misma ciudad, así que evaluamos con 1
+                return 1
+            else: # si la ciudad no está agregada, la agregamos
                 ciudades.append(ciudad_del_estadio)
                 
     # El siguiente criterio de evaluación es la distancia que hay entre estadios.
-    # Procuraremos que la distancia recorrida de los equipos por jornada sea lo más equitativa posible.
-    
-    pass
+    # Procuraremos que la distancia recorrida de los visitantes por jornada sea lo más equitativa posible (minimizando la varianza de las distancias).
+    var_jornadas = []
+    for jornada in calendario:
+        distancias = [] # para cada jornada sacaremos las distancias que recorren los visitantes de cada partido.
+        for partido in jornada['partidos']:
+            distancia = calcular_distancia(partido[0], partido[1])
+            distancias.append(distancia)
+        var_dist_jor = np.var(distancias) # sacamos la varianza del vector de distancias de la jornada
+        var_jornadas.append(var_dist_jor)
+        
+    # queremos evaluar mejor al calendario que menor varianza promedio muestre.
+    return -(np.mean(var_jornadas))
 
 # Función para cruzar dos calendarios
 #Ejemplo de variable calendario:
-    #calendario = [
-    #    {"jornada": 1, "partidos": [["Equipo1", "Equipo2", "EstadioA"], ["Equipo3", "Equipo4", "EstadioB"]]},
-    #    {"jornada": 2, "partidos": [["Equipo2", "Equipo3", "EstadioC"], ["Equipo4", "Equipo1", "EstadioD"]]}
-    #]
+    calendario = [
+        {"jornada": 1, "partidos": [["Equipo1", "Equipo2", "EstadioA"], ["Equipo3", "Equipo4", "EstadioB"]]},
+        {"jornada": 2, "partidos": [["Equipo2", "Equipo3", "EstadioC"], ["Equipo4", "Equipo1", "EstadioD"]]}
+    ]
 def cruzar_calendarios(calendario1, calendario2):
     # Crear un calendario vacío 
     nuevo_calendario = []
@@ -109,7 +119,7 @@ def algoritmo_evolutivo(num_generaciones, tamano_poblacion, tasa_cruce, tasa_mut
         poblacion_evaluada = [(calendario, evaluar_calendario(calendario)) for calendario in poblacion]
 
         # Seleccionar a los mejores calendarios
-        poblacion_evaluada.sort(key=lambda x: x[1], reverse=True)
+        poblacion_evaluada.sort(key=lambda x: x[1], reverse=False)
         mejores_calendarios = [cal[0] for cal in poblacion_evaluada[:int(tamano_poblacion * 0.2)]]
 
         nueva_generacion = []
@@ -132,4 +142,55 @@ mejor_calendario, mejor_aptitud = algoritmo_evolutivo(num_generaciones=100, tama
 print("Mejor calendario encontrado:")
 print(mejor_calendario)
 print("Aptitud del mejor calendario:", mejor_aptitud)
+
+#########################
+#ejemplo calendario con equipos y estadios del csv
+calendario = [
+    {
+        "jornada": 1,
+        "partidos": [
+            ["A.C Milan", "Bologna", "Stadio Diego Armando Maradona"],
+            ["Inter de Milan", "Fiorentina", "Mapei Stadium"],
+            ["Roma", "Juventus", "Olimpico di Roma"],
+            ["Lazio", "Verona", "Carlo Castellani"],
+            ["Napoli", "Sassuolo", "Atleti Azzurri d'Italia"],
+            ["Salernitana", "Empoli", "Stadio Via del Mare"],
+            ["Torino", "Atalanta", "Stadio Olimpico Grande Torino"],
+            ["Udinese", "Sassuolo", "Stadio Friuli"],
+            ["Genoa", "Lecce", "Luigi Ferraris"],
+            ["Monza", "Cagliari", "Stadio Brianteo"]
+        ]
+    },
+    {
+        "jornada": 2,
+        "partidos": [
+            ["A.C Milan", "Inter de Milan", "San Siro Stadium"],
+            ["Bologna", "Roma", "Stadio Renato Dall'Ara"],
+            ["Fiorentina", "Lazio", "Artemio Franchi"],
+            ["Juventus", "Napoli", "Allianz Stadium"],
+            ["Verona", "Salernitana", "Marcantonio Bentegodi"],
+            ["Empoli", "Torino", "Stadio Via del Mare"],
+            ["Atalanta", "Udinese", "Atleti Azzurri d'Italia"],
+            ["Sassuolo", "Genoa", "Mapei Stadium"],
+            ["Lecce", "Monza", "Stadio Friuli"],
+            ["Cagliari", "Frosinone", "Unipol Domus"]
+        ]
+    },
+    {
+        "jornada": 3,
+        "partidos": [
+            ["A.C Milan", "Bologna", "Stadio San Siro"],
+            ["Inter de Milan", "Fiorentina", "Stadio Diego Armando Maradona"],
+            ["Roma", "Juventus", "Stadio Olimpico"],
+            ["Lazio", "Verona", "Stadio Bentegodi"],
+            ["Napoli", "Sassuolo", "Carlo Castellani"],
+            ["Salernitana", "Empoli", "Stadio Via del Mare"],
+            ["Torino", "Atalanta", "Stadio Olimpico Grande Torino"],
+            ["Udinese", "Sassuolo", "Stadio Friuli"],
+            ["Genoa", "Lecce", "Stadio Luigi Ferraris"],
+            ["Monza", "Cagliari", "Stadio Brianteo"]
+        ]
+    }
+]
+
 
